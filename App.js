b4d05b76c2e821f41829be335238121c36016edc
Neil Schultz-Cox <neiltcox@icloud.com>
2019-02-21 21:25:35 -0800

    rename tabs

--- App.js
+++ App.js
@@ -2,29 +2,23 @@ import React from 'react';
 import { StyleSheet, Text, View , Dimensions} from 'react-native';
 import { TabView, TabBar, SceneMap } from 'react-native-tab-view';
 
-const FirstRoute = () => (
+const TodayRoute = () => (
   <View style={[styles.scene, { backgroundColor: '#ff4081' }]} />
 );
-const SecondRoute = () => (
+const HistoryRoute = () => (
   <View style={[styles.scene, { backgroundColor: '#673ab7' }]} />
 );
-
-// export default class App extends React.Component {
-//   render() {
-//     return (
-//       <View style={styles.container}>
-//         <Text>Open up App.js to start working on your app!</Text>
-//       </View>
-//     );
-//   }
-// }
+const TipsRoute = () => (
+  <View style={[styles.scene, { backgroundColor: '#303ab7' }]} />
+);
 
 export default class TabViewExample extends React.Component {
   state = {
     index: 0,
     routes: [
-      { key: 'first', title: 'First' },
-      { key: 'second', title: 'Second' },
+      { key: 'today', title: 'Today' },
+      { key: 'history', title: 'History' },
+      { key: 'tips', title: 'Tips' },
     ],
   };
  
@@ -33,8 +27,9 @@ export default class TabViewExample extends React.Component {
       <TabView
         navigationState={this.state}
         renderScene={SceneMap({
-          first: FirstRoute,
-          second: SecondRoute,
+          today: TodayRoute,
+          history: HistoryRoute,
+          tips: TipsRoute,
         })}
         onIndexChange={index => this.setState({ index })}
         initialLayout={{
