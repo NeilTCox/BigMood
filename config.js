5815df91b5c48d5c58fb1f2ba3e93eda51e667c6
Neil Schultz-Cox <neiltcox@icloud.com>
2019-03-07 21:40:39 -0800

    new ngrok

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
-export const endpoint = 'http://c545b849.ngrok.io';
+export const endpoint = 'http://8c07596f.ngrok.io';
 export const fitEndpoint = 'https://www.googleapis.com/fitness/v1';
 export const apikey = 'ya29.GlvEBgYPDCcKaim-VL1iFEe7lhlgUGP-bFzW5tS8fvI0U-QYTV5bUr_Y7z2rFBnGEAWL4bDLUcyWoiYbFTFhLCXQa2crQxM3LoL8VzF_d4HxwqRl97Wqmszxshuk';
 export const email = 'test@test.com'
