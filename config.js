5c9c602b39c8301388798e90c759732e985ecf23
John Christopher Reyes <reyesjohncq@gmail.com>
2019-03-07 18:17:55 -0800

    better apihelper

--- config.js
+++ config.js
@@ -1,2 +1,3 @@
-export const endpoint = 'http://1da417a0.ngrok.io';
+export const endpoint = 'http://c545b849.ngrok.io';
+export const fitEndpoint = 'https://www.googleapis.com/fitness/v1';
 export const apikey = 'ya29.GlvEBgYPDCcKaim-VL1iFEe7lhlgUGP-bFzW5tS8fvI0U-QYTV5bUr_Y7z2rFBnGEAWL4bDLUcyWoiYbFTFhLCXQa2crQxM3LoL8VzF_d4HxwqRl97Wqmszxshuk';
