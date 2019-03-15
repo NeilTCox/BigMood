8e0cee00b98994e4c27c0815860f3f19a82c985a
Neil Schultz-Cox <neiltcox@icloud.com>
2019-03-14 20:46:17 -0700

    calendar done

--- components/ActivityCalendar.js
+++ components/ActivityCalendar.js
@@ -24,6 +24,25 @@ export default class ActivityCalendar extends Component {
     this.calendarHighlights()
   }
 
+  getMoodColor(mood){
+    color = ''
+    switch(mood){
+      case 'HAPPY':
+        color = '#B3DEA9';
+        break;
+      case 'NEUTRAL':
+        color = '#F7CF6D';
+        break;
+      case 'SAD':
+        color = '#EE9B67';
+        break;
+      default:
+        color = '#FFFFFF';
+        break;
+    }
+    return color
+  }
+
   calendarHighlights(){
     // retrieve calendar highlights from server
     callApi(`/days`, 'GET', {}, {email: config.email})
@@ -32,43 +51,33 @@ export default class ActivityCalendar extends Component {
       days = res.days
       
       for(i = 0; i < days.length; i++){
-        color = ''
-        switch(days[i].mood){
-          case 'HAPPY':
-            color = '#B3DEA9';
-            break;
-          case 'NEUTRAL':
-            color = '#EE9B67';
-            break;
-          case 'SAD':
-            color = '#F7CF6D';
-            break;
-          default:
-            color = '#FFFFFF';
-            break;
-        }
+        color = this.getMoodColor(days[i].mood);
 
-        startingDay = true
-        endingDay = true
+        startingDay = true;
+        endingDay = true;
         if(i === 0){
           if(days[i].mood === days[i+1].mood){
-            endingDay = false
+            endingDay = false;
           }
         }else if(i === days.length-1){
           if(days[days.length-1].mood === days[days.length-2].mood){
-            startingDay = false
+            startingDay = false;
           }
         }else{
           if(days[i].mood === days[i-1].mood){
-            startingDay = false
+            startingDay = false;
           }
           if(days[i].mood === days[i+1].mood){
-            endingDay = false
+            endingDay = false;
           }
         }
-
-        calendarHighlights[days[i].date.slice(0, 10)] = {color, startingDay, endingDay}
+        calendarHighlights[days[i].date.slice(0, 10)] = {color, startingDay, endingDay};
       }
+      // today = new Date();
+      // today = String(today.getFullYear()+"-"+('0' + (today.getMonth() + 1)).slice(-2)+"-"+today.getDate());
+      // selectedCalendarHighlights = calendarHighlights
+      // selectedCalendarHighlights[today].selected = true;
+      // selectedCalendarHighlights[today].selectedColor = 'blue';
       this.setState({calendarHighlights : calendarHighlights});
     });
     // dummy data
@@ -84,23 +93,35 @@ export default class ActivityCalendar extends Component {
   displayDate(day){
     callApi('/days/log', 'GET', {}, {email: config.email, date: day.dateString})
     .then((res) => {
-      console.log(res)
-      console.log(res.events)
       date = `${day.month}/${day.day}/${day.year}`;
+      // dateSelected = `${day.year}-${('0' + (day.month + 1)).slice(-2)}-${day.day}`;
+      // console.log(dateSelected);
+      // selectedCalendarHighlights = this.state.unselectedCalendarHighlights;
+      // selectedCalendarHighlights[dateSelected][selected] = true;
+      // selectedCalendarHighlights[dateSelected][selectedColor] = 'blue';
       mood = res.mood;
-      events = res.events;
-      sleep = res.log.info.sleep;
-      steps = res.log.info.steps;
-
-      console.log(date);
-      console.log(mood);
-      console.log(sleep);
-      console.log(steps);
-      for(var event of events){
-        console.log(event);
+      sleep = res.info.sleep;
+      steps = res.info.steps;
+      happyEvents = "";
+      okEvents = "";
+      sadEvents = "";
+      for(var event of res.events){
+        if(event.mood == "HAPPY"){
+          happyEvents += (event.name+", ");
+        }else if(event.mood == "NEUTRAL"){
+          okEvents += (event.name+", ");
+        }else{
+          sadEvents += (event.name+", ");
+        }
       }
 
-      this.setState({date, mood, events, sleep, steps});
+      moodColor = this.getMoodColor(mood);
+
+      happyEvents = happyEvents.slice(0,happyEvents.length-2);
+      okEvents = okEvents.slice(0,okEvents.length-2)
+      sadEvents = sadEvents.slice(0,sadEvents.length-2)
+
+      this.setState({date, mood, moodColor, happyEvents, okEvents, sadEvents, sleep, steps});
     });
   }
 
@@ -125,27 +146,27 @@ export default class ActivityCalendar extends Component {
           markingType = {'period'}
         />
         <View>
-          <Text>
-            Day:{"\n"}
-            {this.state.date}
-          </Text>
-          <Text>
-            Mood:{"\n"}
-            {this.state.mood}
-          </Text>
-          <FlatList
-            data = {this.state.events}
-            keyExtractor={(item, index) => item._id}
-            renderItem = {({item}) => <Text>Activity list:{"\n"}{item.name}</Text>}
-          />
-          <Text>
-            Sleep Amount:{"\n"}
-            {this.state.sleep}
-          </Text>
-          <Text>
-            Step Count:{"\n"}
-            {this.state.steps}
-          </Text>
+          <View style={styles.dayHeader}>
+            <Text style={styles.date}>
+              {this.state.date}
+            </Text>
+            <View style={[styles.mood, {backgroundColor: this.state.moodColor}]}></View>
+          </View>
+          <View style={styles.statsContainer}>
+            <View>
+              <Text style={styles.sleepemoji}>🛏️</Text>
+              <Text style={styles.sleep}>{this.state.sleep} Hours</Text>
+            </View>
+            <View>
+              <Text style={styles.stepsemoji}>👟</Text>
+              <Text style={styles.steps}>{this.state.steps} Steps</Text>
+            </View>
+          </View>
+          <View style={styles.activities}>
+            <Text>😋: {this.state.happyEvents}</Text>
+            <Text>🙂: {this.state.okEvents}</Text>
+            <Text>😥: {this.state.sadEvents}</Text>
+          </View>
         </View>
       </View>
     );
@@ -158,6 +179,64 @@ const styles = StyleSheet.create({
   },
   calendarlist: {
     marginTop: 22,
+  },
+  dayHeader: {
+    flex: 1,
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    height: 200,
+  },
+  date: {
+    marginTop: 10,
+    marginLeft: 10,
+    width: 225,
+    height: 50,
+    fontSize: 40,
+    textAlign: 'center',
+    fontWeight: 'bold',
+  },
+  mood: {
+    backgroundColor: "#FFFFFF",
+    height: 124,
+    width: 124,
+    borderRadius: 124/2,
+    marginRight: 40,
+    marginTop: 40,
+  },
+  statsContainer: {
+    flex: 1,
+    flexDirection: 'row',
+    justifyContent: 'space-around',
+    width: 200,
+  },
+  sleepemoji: {
+    marginTop: 75,
+    marginLeft: 25,
+    width: 75,
+    height: 75,
+    fontSize: 66,
+  },
+  sleep: {
+    marginLeft: 25,
+    width: 75,
+    height: 25,
+    textAlign: 'center',
+  },
+  stepsemoji: {
+    marginTop: 75,
+    width: 75,
+    height: 75,
+    fontSize: 66,
+  },
+  steps: {
+    width: 75,
+    height: 25,
+    textAlign: 'center',
+  },
+  activities: {
+    marginTop: 200,
+    marginLeft: 25,
+    marginRight: 25,
   },
 });
 
