eeb72dfd065a5b44151b78728df58e3cd55c6a4a
Brandon Delehoy <bdelehoy@yahoo.com>
2019-03-15 17:04:53 -0700

    Change SuggestionCenter to SectionList

--- components/SuggestionCenter.js
+++ components/SuggestionCenter.js
@@ -1,21 +1,24 @@
 import React, { Component } from 'react';
-import { TouchableHighlight, FlatList, StyleSheet, Text, View} from 'react-native';
+import { TouchableHighlight, FlatList, SectionList, StyleSheet, Text, View} from 'react-native';
 import { callApi, getTodayHealth } from '../libs/apihelper.js';
 const config = require('../config');
+
+showList = false;
 
 export default class SuggestionCenter extends Component {
   constructor(props) {
     super(props)
-    this.state = { events: [] }
-    //this.state = { events: [], activities: [] }
+    //this.state = { events: [] }
+    this.state = { events: [], activities: [] }
   }
 
   giveSuggestion() {
     // maybe have a check here to prevent API call spam?
+    showList = true
     this._getData().then((res) => {
       console.log(res)
-      this.setState({ events: res.eventSuggestions.concat(res.activitySuggestions) });
-      //this.setState({ events: res.eventSuggestions, activities: res.activitySuggestions });
+      //this.setState({ events: res.eventSuggestions.concat(res.activitySuggestions) });
+      this.setState({ events: res.eventSuggestions, activities: res.activitySuggestions });
     });
   }
 
@@ -31,7 +34,7 @@ export default class SuggestionCenter extends Component {
   };
 
   renderSuggestListHeader = () => {
-    if (this.state.events.length === 0) {
+    if (this.state.events.length + this.state.activities.length === 0) {
       <Text style={styles.eventListHeader}>Looks like we don't have enough data to suggest anything yet! Try adding a new event from the Events tab.</Text>
     }
     return <Text style={styles.eventListHeader}>Here's a list of things you can do that have made you happy before:</Text>;
@@ -48,14 +51,26 @@ export default class SuggestionCenter extends Component {
           <Text style={styles.buttonText}>Give me suggestions!</Text>
         </TouchableHighlight>
 
-        <FlatList
+        {/* <FlatList
           style={styles.eventList}
           data={this.state.events}
           renderItem={({item}) => <Text style={styles.eventListItem}>{item.name}</Text>}
-          keyExtractor={(item, index) => item._id}
           ItemSeparatorComponent={this.renderSuggestListSeparator}
           ListHeaderComponent={this.renderSuggestListHeader}
-        />
+          keyExtractor={(item, index) => item._id}
+        /> */}
+        {showList && <SectionList
+          style={styles.eventList}
+          sections={[
+            { title: 'Events', data: this.state.events },
+            { title: 'Activities', data: this.state.activities }
+          ]}
+          renderSectionHeader={ ({section}) => <Text style={styles.sectionHeader}> { section.title } </Text> }
+          renderItem={({item}) => <Text style={styles.eventListItem}>{item.name}</Text>}
+          ItemSeparatorComponent={this.renderSuggestListSeparator}
+          ListHeaderComponent={this.renderSuggestListHeader}
+          keyExtractor={(item, index) => item._id}
+        />}
       </View>
     );
   }
@@ -83,6 +98,13 @@ const styles = StyleSheet.create({
    eventListHeader: {
     textAlign: 'center',
     paddingBottom: 15,
+   },
+   sectionHeader: {
+    backgroundColor : 'lightgrey',
+    fontSize : 20,
+    padding: 5,
+    color: 'black',
+    fontWeight: 'bold'
    },
    eventListItem: {
      padding: 10,
--- config.js
+++ config.js
@@ -1,4 +1,4 @@
-export const endpoint = 'http://e00e3906.ngrok.io';
+export const endpoint = 'http://169.234.27.127:3000';
 export const fitEndpoint = 'https://www.googleapis.com/fitness/v1';
 export const apikey = 'ya29.GlvGBo-W9fQVaWxH04N50vqXoFsRzaLrSyzKgd-KaTAiLvsO-OlNqjQWGDfTyAyoF6ryTNPs_aqCmI2mLf06x_ucI54WQnfaEQ4-aRCklH63fAlJElr74jDEyvQY';
 export const email = 't@t.com'
