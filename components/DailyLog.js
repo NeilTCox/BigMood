69af5aec6e3823c4ef7f5ef442086a70bb536f51
John Christopher Reyes <reyesjohncq@gmail.com>
2019-03-19 17:56:41 -0700

    fix date

--- components/DailyLog.js
+++ components/DailyLog.js
@@ -307,12 +307,21 @@ export default class DailyLog extends Component {
   // =================================================================================
   // OTHER HELPER FUNCTIONS ===========================================================
   makeDateString() {
-    let dateString = '';
-    let date = new Date();
-    dateString += date.getFullYear() + '-';
-    dateString += (date.getMonth()+1) + '-';
-    dateString += date.getDate();
-    return dateString;
+    let today = new Date();
+    let dd = today.getDate();
+    let mm = today.getMonth() + 1; //January is 0!
+    let yyyy = today.getFullYear();
+
+    if (dd < 10) {
+      dd = '0' + dd;
+    }
+
+    if (mm < 10) {
+      mm = '0' + mm;
+    }
+
+    today = yyyy + '-' + mm + '-' + dd;
+    return today;
   }
 }
 
